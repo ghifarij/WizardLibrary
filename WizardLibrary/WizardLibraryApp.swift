9a0d70d1098e9a8e0612a82912f360691d01a9d4
ghifarij <afga.ghifari@gmail.com>
2025-11-23 18:27:53 +0800

    Initial Commit

---
+++ WizardLibrary/WizardLibraryApp.swift
@@ -0,0 +1,17 @@
+//
+//  WizardLibraryApp.swift
+//  WizardLibrary
+//
+//  Created by Afga Ghifari on 23/11/25.
+//
+
+import SwiftUI
+
+@main
+struct WizardLibraryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
